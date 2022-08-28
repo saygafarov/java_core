d57e5a7e6b9d264893c88918436fbe75677b0216
Igor Saigafarov <igor.saigafarov@yandex.ru>
2022-08-28 15:14:06 +0300

    add testInsertionSort

--- Ylab/java_core/src/main/java/org/example/tasktwo/TaskTwo.java
+++ Ylab/java_core/src/main/java/org/example/tasktwo/TaskTwo.java
@@ -5,23 +5,16 @@ import java.util.Arrays;
 public class TaskTwo {
     public static void main(String[] args) {
         int[] arr = new int[]{5, 6, 3, 2, 5, 1, 4, 9};
-        int[] arr2 = new int[]{1, 1, 1, 9, -9, 9, 55, 55, 55, 2, -7, 4, 1, 5, 6, 98, 8, 8, 8, 888888, 123};
-        int[] arr3 = new int[]{0, 0, '0', 0, '4', 0, 0, 50, 0, 0, 0, 0, 110, 0, 1};
-        int[] arr4 = new int[]{};
         TaskTwo.insertionSort(arr);
-        TaskTwo.insertionSort(arr2);
-        TaskTwo.insertionSort(arr3);
-        TaskTwo.insertionSort(arr4);
         System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.toString(arr2));
-        System.out.println(Arrays.toString(arr3));
-        System.out.println(Arrays.toString(arr4));
+
+        TaskTwo.testInsertionSort();
 
     }
 
-    public static void insertionSort(int[] arr) {
+    public static int[] insertionSort(int[] arr) {
         if (arr == null || arr.length == 0) {
-            return;
+            return arr;
         }
         for (var i = 1; i < arr.length; i++) {
             var current = arr[i];
@@ -32,5 +25,19 @@ public class TaskTwo {
             }
             arr[j + 1] = current;
         }
+        return arr;
+    }
+
+    private static void testInsertionSort() {
+        int[] arr1 = new int[]{5, 6, 3, 2, 5, 1, 4, 8};
+        int[] arr2 = new int[]{1, 1, 1, 9, -9, 9, 55, 55, 55, 2, -7, 4, 1, 5, 6, 98, 8, 8, 8, 888888, 123};
+        int[] arr3 = new int[]{0, 0, '0', 0, '4', 0, 0, 50, 0, 0, 0, 0, 110, 0, 1};
+        int[] arr4 = new int[]{};
+
+        assert (Arrays.equals(TaskTwo.insertionSort(arr1), new int[]{1, 2, 3, 4, 5, 5, 6, 8}));
+        assert (Arrays.equals(TaskTwo.insertionSort(arr2), new int[]{-9, -7, 1, 1, 1, 1, 2, 4, 5, 6, 8, 8, 8, 9, 9, 55, 55, 55, 98, 123, 888888}));
+        assert (Arrays.equals(TaskTwo.insertionSort(arr3), new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 48, 50, 52, 110}));
+        assert (Arrays.equals(TaskTwo.insertionSort(arr4), new int[]{}));
+
     }
 }
